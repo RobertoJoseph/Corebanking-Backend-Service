35f06a2ff04b06102fe71418aa78e65a0c37e24e
RobertoJoseph <roberto.josephselim@gmail.com>
2022-08-23 14:37:56 +0200

    Adding loanofficer

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,25 +1,50 @@
 import * as React from "react";
-import Button from "@material-ui/core/Button";
-import SaveIcon from "@material-ui/icons/Save";
-import { Gridit, TextField } from "@material-ui/core";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
 
-import Container from "@material-ui/core/Container";
-import { Paper, Grid, Typography } from "@material-ui/core";
-import { useState, useEffect } from "react";
-
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+
 const initialForm = {
   email: "",
   password: "",
 };
-export default function Auth() {
-  const [branch, setBranch] = useState({});
-  const [role, setRole] = useState("");
-  const [form, setForm] = useState(initialForm);
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function LoanOfficer() {
+  const [form, setForm] = React.useState(initialForm);
   const history = useHistory();
 
-  const handleClick = async () => {
+  const handleSubmit = async () => {
     try {
       const response = await axios.post(
         "http://localhost:8080/tanmeyah/login",
@@ -27,38 +52,68 @@ export default function Auth() {
       );
       if (response.data.role === "ADMIN") {
         history.push("/admin");
-
-      }
+      } else if (response.data.role === "TELLER") {
+      } else if (response.data.role === "LOAN_OFFICER")
+        history.push("/loanofficer");
     } catch (err) {
       console.log(err);
     }
   };
   return (
-    <Paper elevation={3}>
-      <Typography variant="h5">Sign in</Typography>
-      <Grid container spacing={2} justifyContent="center">
-        <Grid justifyContent="center" item md={8}>
-          <TextField
-            label="Email"
-            name="email"
-            type="text"
-            onChange={(e) => setForm({ ...form, email: e.target.value })}
-          ></TextField>
-        </Grid>
-        <Grid item md={8}>
-          <TextField
-            label="Password"
-            name="password"
-            type="password"
-            onChange={(e) => setForm({ ...form, password: e.target.value })}
-          ></TextField>
-        </Grid>
-        <Grid item>
-          <Button variant="contained" color="primary" onClick={handleClick}>
-            Submit
-          </Button>
-        </Grid>
-      </Grid>
-    </Paper>
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={(e) => setForm({ ...form, password: e.target.value })}
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleSubmit}
+            >
+              Sign In
+            </Button>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
   );
 }
